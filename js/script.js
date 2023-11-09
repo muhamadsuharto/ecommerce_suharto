19166feff19be0302a22604711d34981367c1b7e
muhamadsuharto <xfsuharto@gmail.com>
2023-11-09 12:36:21 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//togle clas active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
